457631a6065e2b483fdd5332541912fa3e4d67f6
nohasaaed <noha.saaed98@yahoo.com>
2019-03-04 22:21:57 +0200

    Wallet

---
+++ Block.java
@@ -0,0 +1,32 @@
+package sw2_a2;
+import java.util.Date;
+
+
+public class Block {
+	public String hash;//signature
+	public String previousHash;
+	private String data; //our data will be a simple message.
+	private long timeStamp; 
+	private int nonce; ///
+
+	//Block Constructor.
+	public Block(String data,String previousHash ) {
+		this.data = data;
+		this.previousHash = previousHash;
+		this.timeStamp = new Date().getTime();
+		this.hash = calculateHash(); 
+
+	}
+	
+	public String calculateHash() {
+	
+		String calculatedhash = StringUtil.applySha256( 
+				previousHash +
+				Long.toString(timeStamp) +
+				data 
+				);
+		return calculatedhash;
+	}
+	
+	
+}
