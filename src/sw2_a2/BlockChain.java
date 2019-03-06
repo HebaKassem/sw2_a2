e51e7e7f5a0744319871cb2419e49d993bdbbb41
HebaKassem <heba1kassem@yahoo.com>
2019-03-06 09:42:16 +0200

    FixingProjectStructure

--- src/sw2_a2/BlockChain.java
+++ src/sw2_a2/BlockChain.java
@@ -40,6 +40,23 @@ public class BlockChain {
 			BlockChain c= new BlockChain();
 			c.isValid();
 		}
+		
+		
+		
+		Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider()); 
+		//Create the new wallets
+		walletA = new Wallet();
+		walletB = new Wallet(); 
+		//Test public and private keys
+		System.out.println("Private and public keys:");
+		System.out.println(StringUtil.getStringFromKey(walletA.privateKey));
+		System.out.println(StringUtil.getStringFromKey(walletA.publicKey));
+		//Create a test transaction from WalletA to walletB 
+		Transaction transaction = new Transaction(walletA.publicKey, walletB.publicKey, 5, null);
+		transaction.generateSignature(walletA.privateKey);
+		//Verify the signature works and verify it from the public key
+		System.out.println("Is signature verified");
+		System.out.println(transaction.verifiySignature());
 	}
 
 
@@ -76,3 +93,5 @@ public class BlockChain {
 	}
 
 }
+
+	
